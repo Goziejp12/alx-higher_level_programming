622406646bb4406a28e33de80758e47d48c9da59
Goziejp12 <chigoziemaduka401@gmail.com>
2022-12-05 05:07:36 -0800

    commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        new_list = my_list.copy()
+        new_list = list(my_list)
         return new_list
     else:
         my_list[idx] = element
